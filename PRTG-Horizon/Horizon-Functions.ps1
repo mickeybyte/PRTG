88fd8134beb8b549a86be9efd07c9a8da489cce1
Mickeybyte <mickeybyte@gmail.com>
2021-06-01 16:53:21 +0200

    Added -SavePassword option and re-use access tokens between scripts (security & performance increase)

--- PRTG-Horizon/Horizon-Functions.ps1
+++ PRTG-Horizon/Horizon-Functions.ps1
@@ -33,7 +33,7 @@
         return invoke-restmethod -Method Post -uri "$url/rest/login" -ContentType "application/json" -Body ($Credentials | ConvertTo-Json)
     }
 
-    #Close API Connection
+    # Close API Connection
     function Close-HRConnection(){
         param(
             $accessToken,
@@ -41,4 +41,159 @@
         )
         return Invoke-RestMethod -Method post -uri "$url/rest/logout" -ContentType "application/json" -Body ($accessToken.refresh_token | ConvertTo-Json)
     }
+    
+    # Refresh access token
+    function Update-HRConnection(){
+        param(
+            $accessToken,
+            $url
+        )
+        return Invoke-RestMethod -Method post -uri "$url/rest/refresh" -ContentType "application/json" -Body ($accessToken.refresh_token | ConvertTo-Json)
+    }
 
+    # Retrieve token from file
+    function Get-AccessToken() {
+        param(
+            [string]$SecureFile
+        )
+        if (Test-Path -path $SecureFile -PathType Leaf) {
+            $encrypted_data = Get-Content $SecureFile | ConvertTo-SecureString
+            $ptr_data = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($encrypted_data)
+            $result_data = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($ptr_data) | ConvertFrom-Json
+            [System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($ptr_data)
+        }
+        # if file is found, data is returned, else null values are returned
+        return @{access_token = $result_data.accessToken; refresh_token = $result_data.refreshToken} 
+    }
+     
+    # Update access token
+    function Update-AccessToken() {
+        param(
+            [PSCustomObject]$accessToken,
+            [string]$SecureFile
+        )
+
+        if (Test-Path -path $SecureFile -PathType Leaf) {
+            # securefile exists, retrieve current password (if stored)
+            $encrypted_data = Get-Content $SecureFile | ConvertTo-SecureString
+            $ptr_data = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($encrypted_data)
+            $result_data = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($ptr_data) | ConvertFrom-Json
+            [System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($ptr_data)
+        }
+        [PSCustomObject]$data = @{
+            password = $result_data.password
+            accessToken = $accessToken.access_token
+            refreshToken = $accessToken.refresh_token
+        }
+        # convert to JSON string, encrypt and save to file
+        ConvertTo-SecureString $($data | ConvertTo-Json) -AsPlainText -Force | ConvertFrom-SecureString | Out-file $SecureFile
+    }
+
+    # Retrieve password from file
+    function Get-Password() {
+        param(
+            [string]$SecureFile
+        )
+        if (Test-Path -path $SecureFile -PathType Leaf) {        
+            $encrypted_data = Get-Content $SecureFile | ConvertTo-SecureString
+            $ptr_data = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($encrypted_data)
+            $result_data = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($ptr_data) | ConvertFrom-Json
+            [System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($ptr_data)
+        }    
+        # if file is found, return password, else return null
+        return $result_data.password
+    }
+    
+    # Store password in secure file
+    function Update-Password(){
+        param(
+            [string]$pass,
+            [string]$SecureFile
+            )
+        if ([string]::IsNullOrEmpty($pass)){
+            # no password specified!
+            return $false
+        } else {
+            # save password and quit
+            [PSCustomObject]$data = @{}
+
+            # Test if password is correct
+            try {
+                $accessToken = Open-HRConnection -username $HVUser -password $HVPass -domain $HVDomain -url $HVUrl
+                $data.password = $pass
+                if ($SaveToken) {
+                    $data.accessToken = $accessToken.access_token
+                    $data.refreshToken = $accessToken.refresh_token
+                }
+            } Catch {
+                $err = $_.Exception.Response.StatusDescription
+                return $false
+            }
+
+            # convert to JSON string, encrypt and save to file
+            ConvertTo-SecureString $($data | ConvertTo-Json) -AsPlainText -Force | ConvertFrom-SecureString | Out-file $SecureFile
+            return $true
+        }
+    }
+
+    # Process input parameters
+    function process-Parameters() {
+        if ($SavePassword) {
+            # if -SavePassword is given, only save password and exit. No monitoring is done!
+            if (Update-Password -pass $HVPass -secureFile $SecureFile) {
+                # return error: pass saved, remove parameter
+                $csr.Error("Password saved. Please remove -SavePassword parameter now!")
+            } else {
+                # return error: pass not saved, check parameters
+                if ([string]::IsNullOrWhiteSpace($err)){
+                $csr.Error("parameter -SavePassword given without password!")
+                } else {
+                    $csr.Error("Password check failed: $($err)")
+                }
+            }
+            write-host $csr.result()
+            exit
+        }
+        
+        # if password is empty, check if it is stored
+        if ([string]::IsNullOrEmpty($HVPass)) { $HVPass = Get-Password($SecureFile)}
+        # if password is still empty, no password is stored of passed as parameter
+        if ([string]::IsNullOrEmpty($HVPass)) { 
+            $csr.Error("No password supplied or found")
+            write-host $csr.result()
+            exit
+        }
+        
+        # retrieve access token from secure file
+        $accessToken = Get-AccessToken($SecureFile)
+        # Check authentication to REST API
+        try { 
+            # do REST API call to check if access token still valid
+            $test = Invoke-RestMethod -Method Get -uri "$HVurl/rest/monitor/ad-domains" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+            $csr.Text = "Auth: OK"
+        } Catch {
+            # unauthorized, get new access token using refresh token
+            try {
+                $accessToken = Update-HRConnection -url $HVUrl -accessToken $accessToken
+                $csr.Text = "Auth: Refreshed"
+                # if SaveToken, store new tokens
+                if ($SaveToken) { Update-AccessToken -accessToken $accessToken -secureFile $SecureFile
+                    $csr.Text += " & saved" }
+            } Catch {
+                # refresh failed, start login to obtain new access token
+                try {
+                    $accessToken = Open-HRConnection -username $HVUser -password $HVPass -domain $HVDomain -url $HVUrl
+                    $csr.Text = "Auth: New token"
+                    if ($SaveToken) { Update-AccessToken -accessToken $accessToken -secureFile $SecureFile 
+                        $csr.Text += " & saved"}
+                } catch {
+                    # all re-authentication methods failed, raise error and exit
+                    $csr.Error("Failed to authenticate: $($_.Exception.Response.StatusDescription)")
+                    write-host $csr.result()
+                    exit
+                }
+            }
+        }
+
+        return $accessToken
+    }
--- PRTG-Horizon/README.md
+++ PRTG-Horizon/README.md
@@ -18,20 +18,33 @@ Download the [PRTG Custom Lookup files](../PRTG-Lookups/)
 1. Create a new device in PRTG pointing to a Horizon Connection server (or the loadbalancer). 
 2. Add a new sensor: "EXE/Script advanced"
 3. Choose the correct script you want for this sensor
-4. Enter the necessary parameters (make sure you add all mandatory parameters or the script will hang, waiting for userinput for the required parameters). You can use PRTG placeholders: e.g. `-HVUrl https://%host -HVUser %windowsuser -HVPass %windowspassword -HVDomain %windowsdomain`    
+4. Enter the necessary parameters. You can use PRTG placeholders: e.g. `-HVUrl https://%host -HVUser %windowsuser -HVPass %windowspassword -HVDomain %windowsdomain`    
   This wil take the values for user, password, host, ... from the information entered in PRTG on the sensor or parent group. Of course, you can also specify a fixed value in the parameters instead of using PRTG placeholders.
 
 You can also test the scripts by running them manually in powershell. You can either pass the parameters or just run it without parameters and you'll be prompted for the mandatory parameters.
+
+There's an option to store the password in a secure way and retrieve it by the scripts in stead of passing it as a parameter each time you run the script. This can be done by: 
+1. In the sensor settings, enter the password as a parameter and also provide the -SavePassword switch: `-HVUrl https://%host -HVUser %windowsuser -HVDomain %windowsdomain -HVPass %windowspassword -SavePassword`
+2. Let the sensor run at least once. If the password is correct and saved, the sensor will go into error status stating the password has been saved and you need to remove the -SavePassword switch now.
+3. adjust the parameters of the sensor and remove the `-HVPass %windowspassword` and `-SavePassword` parameters. The script will now run using the saved password. The only parameters that are still required now are the url, user and domain. 
+
+By Default, when the script gets an access token from the REST API, it will also be saved in an encrypted way and be re-used by the same and other Horizon PRTG monitoring scripts the next time. This eliminates the need to do a REST API Login request each time one of the scripts is run. By default the access token is valid for 30 minutes and can be renewed with the refresh token, which is valid for 8 hours. If both tokens are expired, a new login request will be done to get new access and refresh tokens. If you don't want to save and re-use the tokens, set the parameter -SaveToken to $false (default = $true).
+
+The password and tokens are saved in an encrypted format that can only be decrypted by the user that saved the password (by default the user that runs the PRTG Probe service) and only on the computer where the password was saved. The data is saved to horizon-functions.dat in the same folder as the horizon-functions.ps1. You can change the location of the file by using the parameter `-SecureFile "path\file.name"`.
 ### List of available scripts
 #### Horizon-Functions.ps1
 This script contains some general functions that are used by all other scripts here. Put it in the same location where you put the other scripts.
 #### PRTG-HorizonCS.ps1
 This script shows the status of the connection servers in your environment in a single sensor, using different channels
+
 ![Connection server status 1](images/PRTG-HorizonCS_1.png)
+
 ![Connection server status 2](images/PRTG-HorizonCS_2.png)
 #### PRTG-HorizonAD.ps1
 This script shows the AD domain status of the connection servers in your environment
+
 ![Connection server AD domain status 1](images/PRTG-HorizonAD_1.png)
 #### PRTG-HorizonEventDB.ps1
 This script shows the status of the event database in your environment
+
 ![Connection server AD domain status 1](images/PRTG-HorizonEventDB_1.png)
